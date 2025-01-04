30eb76f5ccd3985c8ca92f360f7f5d3d65f83451
sivammohapatra1310 <sivammohapatro@gmail.com>
2025-01-04 23:35:29 +0530

    projects section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Education from './components/Education';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Education />
+      <Projects />
     </div>
   );
 }
